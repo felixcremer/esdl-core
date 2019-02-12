9190f4189e673e64f5cc082d6d6bc3beccc289c7
Felix Cremer <felix.cremer@uni-jena.de>
2019-02-12 17:40:25 +0100

    Add possibility to set vh and vv as different variable

--- esdl/providers/sentinel1.py
+++ esdl/providers/sentinel1.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-
+import glob
 import netCDF4
 import numpy
 import re
@@ -10,31 +10,32 @@ from esdl.cube_provider import NetCDFCubeSourceProvider
 
 
 class S1Provider(NetCDFCubeSourceProvider):
-    def __init__(self, cube_config, name='sentinel1', dir=None, resampling_order=None):
+    def __init__(self, cube_config, name='sentinel1', dir=None, resampling_order=None, polarisation=None):
         super(S1Provider, self).__init__(cube_config, name, dir, resampling_order)
         self.old_indices = None
-
+        self.pol = polarisation
     @property
     def variable_descriptors(self):
         return {
-            'sentinel1': {
+            'sentinel1_{}'.format(self.pol): {
                 'source_name': 'Band1',
                 'data_type': numpy.float32,
                 'fill_value': numpy.nan,
                 'units': 'bsi',
-                'long_name': 'Mean total ozone column in dobson units',
-                'standard_name': 'atmosphere_mole_content_of_ozone',
+                'long_name': 'Backscatter of Sentinel-1',
+                'standard_name': 'bsi_{}'.format(self.pol),
                 'references': 'Laeng, A., et al. "The ozone climate change initiative: Comparison of four '
                               'Level-2 processors for the Michelson Interferometer for Passive Atmospheric '
                               'Sounding (MIPAS)." Remote Sensing of Environment 162 (2015): 316-343.',
-                'comment': 'Atmospheric ozone based on the Ozone CCI data.',
-                'url': 'http://www.esa-ozone-cci.org/',
-                'project_name' : 'Ozone CCI',
+                'comment': '',
+                'url': '',
+                'project_name' : 'Copernicus',
             }
         }
 
     def compute_source_time_ranges(self):
-        file_names = os.listdir(self.dir_path)
+        file_names = glob.glob(self.dir_path + "/*" + self.pol + "*.nc")
+        print(self.dir_path + "/*" + self.pol + "*.nc")
         source_time_ranges = list()
         date_pattern = re.compile('(\\d{8}T\\d{6})')
         print(file_names)
@@ -66,3 +67,6 @@ class S1Provider(NetCDFCubeSourceProvider):
         # always read as -9.9. Find out why these values are automatically converted and create a better fix.
         source_image[source_image == -9.9] = numpy.nan
         return numpy.roll(numpy.flipud(source_image), 180, axis=1)
+
+def finder(dir=".", pattern=""):
+    files = os.path.listdir(path)
